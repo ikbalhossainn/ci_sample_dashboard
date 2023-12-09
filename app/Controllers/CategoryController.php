de9c974d54e75ee25a5ce03245ec7cee5b4b79fb
ikbalhossainn <ikbalhossain125@gmail.com>
2023-12-09 11:28:42 +0600

    updated

---
+++ app/Controllers/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\CategoryModel;
+
+
+class CategoryController extends BaseController
+{
+    public function index()
+    {
+        
+    }
+}
