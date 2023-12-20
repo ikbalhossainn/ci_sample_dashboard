877c168dc53ea35214e9dc5ffbc927802644075b
ikbalhossainn <ikbalhossain125@gmail.com>
2023-12-20 10:31:56 +0600

    updated

--- app/Controllers/frontend/RegistrationController.php
+++ app/Controllers/frontend/RegistrationController.php
@@ -3,14 +3,31 @@
 namespace App\Controllers\Frontend;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 use CodeIgniter\API\ResponseTrait;
 use CodeIgniter\RESTful\ResourceController;
+
 
 class RegistrationController extends BaseController
 {
     use ResponseTrait;
     public function registration()
     {
-        echo "Hello";
+        $userModel = new UserModel();
+        $data = [
+            'fname' => $this->request->getVar('fname'),
+            'email' => $this->request->getVar('email'),
+            'password' => password_hash($this->request->getVar('password'),PASSWORD_DEFAULT),
+        ];
+        // print_r($inputs);
+
+        if($userModel->insert($data)){
+            $data['msg'] = "Success";
+            return $this->respond($data);
+        } else{
+            $data['msg'] = "Failed";
+            return $this->respond($data);
+        }
+               
     }
 }
